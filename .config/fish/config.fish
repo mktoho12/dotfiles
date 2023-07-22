23daaeeddf677da1886f1897ce21bca6456011fd
mktoho <mk@mktoho.org>
2023-07-23 02:23:34 +0900

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  eval (/opt/homebrew/bin/brew shellenv)
+  starship init fish | source
+end
+
+# status --is-interactive; and source (nodenv init -|psub)
+# status --is-interactive; and source (rbenv init -|psub)
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
+alias be="bundle exec"
+alias gs="git switch"
+alias config='/usr/bin/git --git-dir=/Users/mktoho/.cfg/ --work-tree=/Users/mktoho'
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# pnpm
+set -gx PNPM_HOME "/Users/mktoho/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mktoho/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/mktoho/Downloads/google-cloud-sdk/path.fish.inc'; end
